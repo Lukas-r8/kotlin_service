c0cbf19a7bbf4b1de85b9216e35e663d31cdbcfd
Lucas Alves Da Silva <lalvesdasilva@wayfair.com>
2022-04-27 23:43:04 +0200

    adding test and github actions config

---
+++ src/test/kotlin/com/helloworld/web/HelloWorldControllerTest.kt
@@ -0,0 +1,10 @@
+package com.helloworld.web
+
+import org.junit.jupiter.api.Test
+
+class HelloWorldControllerTest {
+    @Test
+    fun textShouldBeEqual() {
+        assert("hello"=="hello")
+    }
+}
