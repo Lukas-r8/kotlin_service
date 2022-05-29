8df59e04da389560d7e3cf04057c97131768cc7d
Lucas Alves Da Silva <lalvesdasilva@wayfair.com>
2022-05-29 22:14:45 +0200

    adding some debug logs

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,9 @@
 set -eo pipefail
 
 echo "Running Service...123"
+pwd
+echo "Contents of this dir"
 ls -la
-java -jar /application/helloworld.jar
+echo "contents of previous folder..."
+ls -la ..
+java -jar /helloworld.jar
