e27049a81c35d0d1866679a0e3f76e2f485245e9
Lucas Alves Da Silva <lalvesdasilva@wayfair.com>
2022-09-07 01:12:32 +0200

    update command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,4 @@ echo "Running Service.."
 pwd
 echo "Contents of this dir"
 ls -la
-
-echo "running java in debug mode... 333"
-java -Xdebug -jar ./helloworld.jar
+java -Dspring.profiles.active=prod -jar helloworld.jar
