0d53af87c6f80938c070bd26fb7fb02498e1dc31
Lucas Alves Da Silva <lalvesdasilva@wayfair.com>
2022-09-07 18:06:24 +0200

    make main method static

--- src/main/kotlin/com/helloworld/Application.kt
+++ src/main/kotlin/com/helloworld/Application.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MainApplication
-
-fun main(args: Array<String>) {
-    runApplication<MainApplication>(*args)
+class MainApplication {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runApplication<MainApplication>(*args)
+        }
+    }
 }
