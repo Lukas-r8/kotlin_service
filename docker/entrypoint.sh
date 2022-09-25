5831f9489b3a4a754f6bccd5acc941bdccedde39
Lucas Alves Da Silva <lalvesdasilva@wayfair.com>
2022-09-25 02:31:21 +0200

    add debug manifest

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,7 @@ echo "Running Service.."
 pwd
 echo "Contents of this dir"
 ls -la
+echo "MANIFEST:"
+unzip -p helloworld.jar META-INF/MANIFEST.MF
+echo "Run jar"
 java -Dspring.profiles.active=prod -jar helloworld.jar
