3252b32b60b91c10599d82aa64b505260e770d7a
Lucas Alves Da Silva <lalvesdasilva@wayfair.com>
2022-09-25 14:10:43 +0200

    fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,4 @@
 set -eo pipefail
 
 echo "Running Service.."
-pwd
-echo "Contents of this dir"
-ls -la
-echo "MANIFEST:"
-unzip -p helloworld.jar META-INF/MANIFEST.MF
-echo "Run jar"
 java -Dspring.profiles.active=prod -jar helloworld.jar
